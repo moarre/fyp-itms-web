ad1a1e5215cde9c011a8fa092502d248c814a9db
Ariff <ariffazlan16@gmail.com>
2023-07-23 16:20:05 +0800

    check error email job

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendEmailJob implements ShouldQueue
@@ -37,17 +38,32 @@ class SendEmailJob implements ShouldQueue
      */
     public function handle()
     {
-        Mail::send([], [], function ($message) {
-            $message->from($this->user['program']['coordinator']['email'], $this->user['program']['coordinator']['name']);
-            $message->to($this->user['interndata']['companyEmail'])->subject('PENGESAHAN PENERIMAAN PENEMPATAN LATIHAN INDUSTRI');
-            $message->setBody($this->emailMessage, 'text/html');
-            foreach ($this->attachments as $attachment) {
-                if (is_array($attachment)) {
-                    $message->attachData($attachment['data'], $attachment['name'], $attachment['options']);
-                } else {
-                    $message->attach($attachment);
-                }
-            }
-        });
+        try {
+            Mail::send([], [], function ($message) {
+                // Existing email sending logic here
+                Mail::send([], [], function ($message) {
+                    $message->from($this->user['program']['coordinator']['email'], $this->user['program']['coordinator']['name']);
+                    $message->to($this->user['interndata']['companyEmail'])->subject('PENGESAHAN PENERIMAAN PENEMPATAN LATIHAN INDUSTRI');
+                    $message->setBody($this->emailMessage, 'text/html');
+                    foreach ($this->attachments as $attachment) {
+                        if (is_array($attachment)) {
+                            $message->attachData($attachment['data'], $attachment['name'], $attachment['options']);
+                        } else {
+                            $message->attach($attachment);
+                        }
+                    }
+                });
+            });
+
+            // Log a success message
+            Log::info('Email sent successfully');
+        } catch (\Exception $e) {
+            // Log the exception message
+            Log::error('Error sending email: ' . $e->getMessage());
+
+            // Re-throw the exception to mark the job as failed
+            throw $e;
+        }
+
     }
 }
